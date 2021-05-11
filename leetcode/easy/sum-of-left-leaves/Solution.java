09fea9da25ce3f5b7fc8219511a61335e743dc9e
Mohammed Nadeem <naddym20@gmail.com>
2021-05-11 09:12:32 +0530

    Solve sum-of-left-leaves

---
+++ leetcode/easy/sum-of-left-leaves/Solution.java
@@ -0,0 +1,58 @@
+/**
+Given the root of a binary tree, return the sum of all left leaves.
+
+Example 1:
+        3
+       / \ 
+      9  20
+        /  \
+       15   7
+       
+Input: root = [3,9,20,null,null,15,7]
+Output: 24
+Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+
+Example 2:
+
+Input: root = [1]
+Output: 0
+ 
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+-1000 <= Node.val <= 1000
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        return sumOfLeftLeaves(root, false);
+    }
+    
+    private int sumOfLeftLeaves(TreeNode root, boolean isLeft) {
+        if (root == null) {
+            return 0;
+        }
+        
+        if (isLeft && root.left == null && root.right == null) {
+            return root.val;
+        }
+        
+        return sumOfLeftLeaves(root.left, true) + sumOfLeftLeaves(root.right, false);
+    }
+}
