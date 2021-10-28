5821065641ddab8f3d76f9408120540fc74ed27c
Mohammed Nadeem <naddym20@gmail.com>
2021-10-28 13:32:57 +0530

    Other easy solution to matrix-diagonal-sum problem

---
+++ leetcode/easy/matrix-diagonal-sum/SolutionB.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0, n = mat.length;
+        
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i] + mat[i][n - 1 - i];
+        }
+        
+        if (n % 2 != 0) {
+            sum -= mat[n/2][n/2];
+        }
+        
+        return sum;
+    }
+}
